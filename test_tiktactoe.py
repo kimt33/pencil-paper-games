1d9d97c08fefb77341f41e4545f3bde96b29fc43
Taewon D. Kim <davidkim91@gmail.com>
2022-03-30 22:36:26 -0400

    Add method make_move in TicTacToeGame
    Also changed convention for next_player to 0 for player 1 and 1 for player 2
    because it was easier to increment

--- test_tiktactoe.py
+++ test_tiktactoe.py
@@ -11,4 +11,38 @@ def test_init():
     assert game.avail_positions == set(
         [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
     )
+    assert game.next_player == 0
+
+
+def test_make_move():
+    """Test tictactoe.make_move."""
+    game = tictactoe.TicTacToeGame()
+
+    game.make_move((0, 0))
+    assert game.player1_positions == set([(0, 0)])
     assert game.next_player == 1
+
+    game.make_move((0, 1))
+    assert game.player2_positions == set([(0, 1)])
+    assert game.next_player == 0
+
+    game.make_move((1, 0))
+    assert game.player1_positions == set([(0, 0), (1, 0)])
+    assert game.next_player == 1
+
+    with pytest.raises(tictactoe.GameError):
+        game.make_move((1, 0))
+    assert game.player2_positions == set([(0, 1)])
+    game.make_move((1, 1))
+    assert game.player2_positions == set([(0, 1), (1, 1)])
+    assert game.next_player == 0
+
+    game.make_move((2, 0))
+    assert game.player1_positions == set([(0, 0), (1, 0), (2, 0)])
+    assert game.next_player == 1
+
+    # NOTE: game continues even though player 1 won
+    game.make_move((2, 1))
+    assert game.player2_positions == set([(0, 1), (1, 1), (2, 1)])
+    assert game.next_player == 0
+
--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,11 @@
 """Module for running tic tac toe games."""
 
 
+class GameError(Exception):
+    """Error arising from game of tic tac toe."""
+    pass
+
+
 class TicTacToeGame:
     """Platform for playing tic tac toe game.
 
@@ -14,6 +19,7 @@ class TicTacToeGame:
         Positions available to be occupied.
     next_player : int
         Player that will move next.
+        If `0` then player 1. If `1` then player 2.
 
     Methods
     -------
@@ -28,7 +34,19 @@ class TicTacToeGame:
         self.player1_positions = set([])
         self.player2_positions = set([])
         self.avail_positions = {(i, j) for i in range(3) for j in range(3)}
-        self.next_player = 1
+        self.next_player = 0
 
     def make_move(self, coords):
-        pass
+        coords = tuple(coords)
+        if not self.avail_positions:
+            raise GameError("No more moves available.")
+        if coords not in self.avail_positions:
+            raise GameError("Given position is already occupied.")
+
+        if self.next_player == 0:
+            self.player1_positions.add(coords)
+        else:
+            self.player2_positions.add(coords)
+        self.avail_positions.remove(coords)
+
+        self.next_player = (self.next_player + 1) % 2
