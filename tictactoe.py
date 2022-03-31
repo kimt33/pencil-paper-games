e0fa3e4906378911eef1cc82908eb772128c1ef8
Taewon D. Kim <davidkim91@gmail.com>
2022-03-30 22:11:07 -0400

    Add framework for tictactoe game
    Just basic class structure and docs

---
+++ tictactoe.py
@@ -0,0 +1,29 @@
+"""Module for running tic tac toe games."""
+
+
+class TicTacToeGame:
+    """Platform for playing tic tac toe game.
+
+    Attributes
+    ----------
+    player1_positions : set of 2-tuple of int
+        Positions that player 1 occupies.
+    player2_positions : set of 2-tuple of int
+        Positions that player 2 occupies.
+    next_player : int
+        Player that will move next.
+
+    Methods
+    -------
+    __init__(self)
+        Initialize game.
+    make_move(self, coords)
+        Apply piece for player designated by `next_player`
+
+    """
+    # NOTE: should the game stop condition be implemented here?
+    def __init__(self):
+        pass
+
+    def make_move(self, coords):
+        pass
